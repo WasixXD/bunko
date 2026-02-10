d6987527bdfc62cba10597782ff5ed58dd0c0f2f
WasixXD <wasilewski2020@gmail.com>
2026-02-10 20:24:22 -0300

    feat: cleaning

--- backend/structs/manga.go
+++ backend/structs/manga.go
@@ -40,3 +40,11 @@ type MangaProps struct {
 	Status    string `json:"status"`
 	Url       string `json:"url"`
 }
+
+type MangaPost struct {
+	Name         string `json:"name"`
+	ProviderName string `json:"provider"`
+	TimeRule     string `json:"time_rule"`
+	Url          string `json:"url"`
+	MangaPath    string `json:"manga_path"`
+}
--- backend/structs/structs.go
+++
@@ -1,9 +0,0 @@
-package structs
-
-type MangaPost struct {
-	Name         string `json:"name"`
-	ProviderName string `json:"provider"`
-	TimeRule     string `json:"time_rule"`
-	Url          string `json:"url"`
-	MangaPath    string `json:"manga_path"`
-}
